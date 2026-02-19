e6021d103fe7ea980a3249965f00d53684b7b5ad
CrazzeDill <68263662+CrazzeDill@users.noreply.github.com>
2026-02-20 00:26:48 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://crazzedill.github.io',
-    base : '/arknights-resource-hub',
+    base: '/arknights-resource-hub',
     trailingSlash: 'never'
 });
