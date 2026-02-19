112c64134c233a8750a875c1288187facde2e0f3
CrazzeDill <68263662+CrazzeDill@users.noreply.github.com>
2026-02-20 00:18:49 +0700

    Set site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://crazzedill.github.io/',
+    base : '/arknights-resource-hub',
     trailingSlash: 'never'
 });
